aa9b5be9b944813e07d6de72ce4b56dc2dca51cb
Gamaliel Bernal <gamalielbj@gmail.com>
2021-02-24 16:32:01 -0600

    Update alimentos.routes.js

--- App/Backend/routes/alimentos.routes.js
+++ App/Backend/routes/alimentos.routes.js
@@ -12,6 +12,18 @@ router.get('/', async (req, res) => {
     res.send(alimentosLista);
 });
 
+router.get('/:id', async (req, res) => {
+
+    const alimento = await Alimentos.findById(req.params.id);
+
+    if (!alimento) {
+        res.status(500).json({ succes: false, message: 'No existe ese alimento :/' });
+    } else {
+        res.send(alimento);
+    }
+
+});
+
 router.post('/', (req, res) => {
 
     const alimento = new Alimentos({
@@ -44,6 +56,55 @@ router.post('/', (req, res) => {
         .catch((err) => {
             res.status(500).json({ error: err, success: false })
         })
+});
+
+router.put('/:id', async (req, res) => {
+
+    const alimentoEditar = await Alimentos.findOneAndUpdate(req.params.id, {
+        nombreAlimento: req.body.nombreAlimento,
+        imagen: req.body.imagen,
+        grupoExportable: req.body.grupoExportable,
+        subGrupoExportable: req.body.subGrupoExportable,
+        clasificacionExportable: req.body.clasificacionExportable,
+        grupoAlimento: req.body.grupoAlimento,
+        mensaje: req.body.mensaje,
+        icono: req.body.icono,
+        opcionesPreparacion: req.body.opcionesPreparacion,
+        cantidadAlimento: req.body.cantidadAlimento,
+        caloriasMacronutrientes: req.body.caloriasMacronutrientes,
+        vitaminas: req.body.vitaminas,
+        minerales: req.body.minerales,
+        aspectoGlucemico: req.body.aspectoGlucemico,
+        aspectoMedioambiental: req.body.aspectoMedioambiental,
+        aspectoEconomico: req.body.aspectoEconomico,
+        componentesBioactivos: req.body.componentesBioactivos,
+        aditivosAlimentarios: req.body.aditivosAlimentarios,
+        atributosAdicionales: req.body.atributosAdicionales,
+        marca: req.body.marca
+    }, {
+        new: true
+    });
+
+    if (!alimentoEditar) {
+        return res.status(404).send('El producto no se encontró o no se pudo editar :c');
+    } else {
+        res.status(200).send(alimentoEditar);
+    }
+
+});
+
+router.delete('/:id', (req, res) => {
+    Alimentos.findByIdAndRemove(req.params.id)
+        .then(alimento => {
+            if (alimento) {
+                return res.status(200).json({ success: true, message: 'Alimento eliminado :D' });
+            } else {
+                return res.status(404).json({ success: true, message: 'Alimento no encontrado :/' });
+            }
+        })
+        .catch(err => {
+            return res.status(400).json({ succes: false, error: err });
+        });
 });
 
 module.exports = router;
