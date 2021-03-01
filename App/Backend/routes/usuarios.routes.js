be56194aa2a6093b608cf66ccfc895c295c78800
Gamaliel Bernal <gamalielbj@gmail.com>
2021-03-01 14:02:57 -0600

    Auth with JWT.
    1h token.

--- App/Backend/routes/usuarios.routes.js
+++ App/Backend/routes/usuarios.routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 router.get('/', async (req, res) => {
 
@@ -63,4 +64,65 @@ router.post('/', async (req, res) => {
     res.send(crearUsuario);
 });
 
+router.post('/login', async (req, res) => {
+
+    const usuario = await Usuarios.findOne({ email: req.body.email });
+    const SECRET = process.env.SECRET;
+
+    if (!usuario) {
+        return res.status(400).send('Usuario no registrado :c');
+    }
+
+    if (usuario && bcrypt.compareSync(req.body.contrasena, usuario.contrasena)) {
+        const token = jwt.sign(
+            {
+                userId: usuario.id
+            }, SECRET, { expiresIn: '1h' }
+        )
+        res.status(200).send({ usuario: usuario.email, token: token });
+    } else {
+        res.status(400).send('Contraseña incorrecta');
+    }
+});
+
+router.put('/:id', async (req, res) => {
+
+    let editarUsuario = await Usuarios.findOneAndUpdate(req.params.id, {
+        logros: req.body.logros,
+        nombre: req.body.nombre,
+        foto: req.body.foto,
+        email: req.body.email,
+        fechaDeNacimiento: req.body.fechaDeNacimiento,
+        contrasena: bcrypt.hashSync(req.body.contrasena, 10),
+        genero: req.body.genero,
+        peso: req.body.peso,
+        altura: req.body.altura,
+        actividadFisica: req.body.actividadFisica,
+        historiaClinica: req.body.historiaClinica,
+        nivelSocioeconomico: req.body.nivelSocioeconomico,
+        comidaFavorita: req.body.comidaFavorita,
+        comidaNoFavorita: req.body.comidaNoFavorita,
+        alergiasAlimentarias: req.body.alergiasAlimentarias,
+        meta: req.body.meta,
+        tipoDeUsuario: req.body.tipoDeUsuario,
+        extras: req.body.extras,
+        desayuno: req.body.desayuno,
+        colacion1: req.body.colacion1,
+        comida: req.body.comida,
+        colacion2: req.body.colacion2,
+        cena: req.body.cena,
+        desayunoAyer: req.body.desayunoAyer,
+        colacion1Ayer: req.body.colacion1Ayer,
+        comidaAyer: req.body.comidaAyer,
+        colacion2Ayer: req.body.colacion2Ayer,
+        cenaAyer: req.body.cenaAyer
+    });
+
+    editarUsuario = await editarUsuario.save();
+
+    if (!editarUsuario)
+        return res.status(400).send('No se pudo editar el usuario :c');
+
+    res.send(editarUsuario);
+});
 module.exports = router;
