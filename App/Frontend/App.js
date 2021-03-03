97e61f6b74f78d5ccc0b8a52bcbafd9a9905399c
Gamaliel Bernal <gamalielbj@gmail.com>
2021-03-03 14:28:58 -0600

    Icons added to nav.

--- App/Frontend/App.js
+++ App/Frontend/App.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { SafeAreaView, ScrollView } from "react-native";
+import { SafeAreaView, ScrollView, TouchableOpacity, View } from "react-native";
 import { NavigationContainer } from '@react-navigation/native'
+import { FontAwesome5 } from '@expo/vector-icons'
 
 // Components
 import AlimentosContainer from './src/screens/Alimentos/AlimentosContainer'
 import Banner from './src/components/Banner'
 
-// Navigator
+// Navigators
 import Main from './src/navigators/Main'
-
+import HamburgerMain from './src/navigators/HamburgerMain'
 
 export default function App() {
   return (
--- App/Frontend/src/navigators/Main.js
+++ App/Frontend/src/navigators/Main.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Icon } from 'native-base';
+import Icon from 'react-native-vector-icons/FontAwesome'
+import Icon2 from 'react-native-vector-icons/MaterialCommunityIcons'
+import Icon3 from 'react-native-vector-icons/FontAwesome5'
 
 // Screens - Stacks ?
 import AlimentosContainer from '../screens/Alimentos/AlimentosContainer'
@@ -11,16 +13,53 @@ import MensajesContainer from '../screens/Mensajes/MensajesContainer'
 import RegistroDietetico from '../screens/Registro/RegistroDietetico'
 
 const Tab = createBottomTabNavigator();
+const primaryColor = '#439776';
 
 function Main() {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={AlimentosContainer} />
-            <Tab.Screen name="Registro" component={RegistroDietetico} />
-            <Tab.Screen name="Aprendiendo" component={AprendiendoContainer} />
-            <Tab.Screen name="Comunidad" component={ComunidadContainer} />
-            <Tab.Screen name="Perfil" component={Perfil} />
-            <Tab.Screen name="Mensajes" component={MensajesContainer} />
+        <Tab.Navigator
+            initialRouteName="Principal"
+            tabBarOptions={{
+                showLabel: false,
+                activeTintColor: primaryColor
+            }}
+        >
+            <Tab.Screen name="Principal" component={AlimentosContainer}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <Icon name='home' style={{ position: 'relative' }} color={color} size={48} />
+                    )
+                }} />
+            <Tab.Screen name="Registro" component={RegistroDietetico}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <Icon2 name='silverware-fork-knife' style={{ position: 'relative' }} color={color} size={40} />
+                    )
+                }} />
+            <Tab.Screen name="Aprendiendo" component={AprendiendoContainer}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <Icon2 name='book-open-page-variant' style={{ position: 'relative' }} color={color} size={40} />
+                    )
+                }} />
+            <Tab.Screen name="Comunidad" component={ComunidadContainer}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <Icon3 name='users' style={{ position: 'relative' }} color={color} size={40} />
+                    )
+                }} />
+            <Tab.Screen name="Perfil" component={Perfil}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <Icon name='user-circle' style={{ position: 'relative' }} color={color} size={40} />
+                    )
+                }} />
+            <Tab.Screen name="Mensajes" component={MensajesContainer}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <Icon name='comments' style={{ position: 'relative' }} color={color} size={40} />
+                    )
+                }} />
         </Tab.Navigator>
     );
 }
