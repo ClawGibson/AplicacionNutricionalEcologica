2d1867144c20c992d9b08cd863b7b5083a58946c
Gamaliel Bernal <gamalielbj@gmail.com>
2021-03-08 15:27:17 -0600

    MenusBase updated
    Added new fields in ingredientes.
    - alimento (string).
    - cantidad (number).

--- App/Backend/models/MenusBase.js
+++ App/Backend/models/MenusBase.js
@@ -3,14 +3,19 @@ const { Schema, model, mongo } = require('mongoose');
 const menusBaseSchema = new Schema({
     titulo: { type: String, required: true },
     imagen: { type: String, required: false },
-    ingredientes: [String],
+    ingredientes: [{
+        alimento: { type: String, required: true },
+        cantidad: { type: Number, required: true }
+    }],
     categoria: { type: String, required: true }
 });
 
-menusBaseSchema.method('toJSON', function () {
-    const { __v, ...object } = this.toObject();
-    const { _id: id, ...result } = object;
-    return { ...result, id };
+menusBaseSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+menusBaseSchema.set('toJSON', {
+    virtuals: true
 });
 
 module.exports = model('MenusBase', menusBaseSchema);
