cb3954d871cf4c423b9af885f52df429fdd0d440
Gamaliel Bernal <gamalielbj@gmail.com>
2021-03-09 17:59:48 -0600

    Update App.js

--- App/Frontend/App.js
+++ App/Frontend/App.js
@@ -5,13 +5,8 @@ import { NavigationContainer } from '@react-navigation/native'
 import { Provider } from 'react-redux'
 import Store from './src/Redux/Store'
 
-// Components
-import AlimentosContainer from './src/screens/Alimentos/AlimentosContainer'
-import Banner from './src/components/Banner'
-
 // Navigators
 import Main from './src/navigators/Main'
-import HamburgerMain from './src/navigators/HamburgerMain'
 
 export default function App() {
   return (
