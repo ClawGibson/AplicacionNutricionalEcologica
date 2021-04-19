ef05374becf1be75a07914dd1c71dbcb3b93c5ff
Gamaliel Bernal <gamalielbj@gmail.com>
2021-04-19 18:37:46 -0500

    Admin profile created
    Only UI desing.
    
    Added to assets/data/cloud the standard avatar.

--- App/Frontend/src/assets/data/cloud.js
+++ App/Frontend/src/assets/data/cloud.js
@@ -5,3 +5,4 @@ export const hojaTriste = 'https://res.cloudinary.com/dwjv6orjf/image/upload/v16
 export const gotaTriste = 'https://res.cloudinary.com/dwjv6orjf/image/upload/v1616609956/tristeGota_wqsbao.png';
 export const hojaFeliz = 'https://res.cloudinary.com/dwjv6orjf/image/upload/v1616609956/felizHoja_h86wj3.png';
 export const gotaFeliz = 'https://res.cloudinary.com/dwjv6orjf/image/upload/v1616609956/felizGota_mxwgyr.png';
+export const standardAvatar = 'https://res.cloudinary.com/dwjv6orjf/image/upload/v1618875313/standard_avatar_txfgx5.png';
