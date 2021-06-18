2395df82b0deb0b05182904f424568014a8e8885
Gamaliel Bernal <gamalielbj@gmail.com>
2021-06-18 18:54:51 -0500

    Achievements & User Achievements

---
+++ App/Backend/routes/logrosDeUsuario.routes.js
@@ -0,0 +1,7 @@
+const LogrosDeUsuario = require("../models/LogrosDeUsuario");
+const express = require("express");
+const router = expres.Router();
+
+router.get("/", async (req, res) => {});
+
+module.exports = router;
